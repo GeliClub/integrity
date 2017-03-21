ad9ac40d1d3e4612aca52cd9d0d4f3a3e27f08b5
Nick Li <hli109@hawk.iit.edu>
2017-03-21 10:26:22 -0500

    getting started, added manual controls for pcd processing

--- src/Main/src/MAIN.cpp
+++ src/Main/src/MAIN.cpp
@@ -47,11 +47,10 @@ int main (int argc, char** argv)
   ofstream outputFile("output_for_matlab.txt");
 
   // To get the pose
-  std::vector<Eigen::Matrix4d> T= read_transformations();  
+  std::vector<Eigen::Matrix4d> T= read_transformations();
 
   // Need two clouds, original cloud to plot and cloud to work on
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>),
-  								 cloud_original (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), cloud_original (new pcl::PointCloud<pcl::PointXYZ>);
 
   // clusters
   std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr > clusters;
@@ -60,11 +59,11 @@ int main (int argc, char** argv)
   // Initialize viwer
   pcl::visualization::PCLVisualizer viewer("PCL Viewer");
   viewer.setBackgroundColor( 0.0, 0.0, 0.0 );
-  // viewer.setFullScreen(true); 
+  // viewer.setFullScreen(true);
   viewer.setCameraPosition(-21.1433, -23.4669, 12.7822,  // do not change unless you know how
                            0.137915, -0.429331, -1.9301,
                            0.316165, 0.28568, 0.904669);
-  viewer.setCameraClipDistances(0.0792402, 79.2402); 
+  viewer.setCameraClipDistances(0.0792402, 79.2402);
   std::string cloud_cylinder_id;
 
   for (int i= 0; i <= parameters.numFrames; ++i)
@@ -72,7 +71,7 @@ int main (int argc, char** argv)
 
   	//----------------------------  READ DATA  ----------------------------//
   	// Form the name to read the data
-  	std::string filename2; 
+  	std::string filename2;
   	std::stringstream sa;
   	sa << setw(6) << setfill('0') << i;
   	filename2= sa.str();
@@ -81,7 +80,7 @@ int main (int argc, char** argv)
   	pcl::PCDReader reader;
   	std::cout << "File currently being used :"<< filename2 << ".pcd" << endl;
   	reader.read ("../Data/pcd-files/KITTI/" + filename2 + ".pcd", *cloud_original);
-  	std::cout << "PointCloud before filtering has: " 
+  	std::cout << "PointCloud before filtering has: "
               << cloud_original->points.size () << " data points." << std::endl; //*
   	//----------------------------  READ DATA  ----------------------------//
 
@@ -92,17 +91,17 @@ int main (int argc, char** argv)
   	// Eliminate far-away points
   	extracting_far_away_points(cloud, parameters);
 
-  	// Clustering 
+  	// Clustering
   	clusters.clear(); // clear the clusters to store new ones
   	cluster_extraction (cloud, clusters, parameters);
 
   	// Extract Planes from clusters so that it is easier to find the cylinders
   	plane_from_cluster (clusters, parameters);
 
-    // Cylinder Segmentation 
+    // Cylinder Segmentation
    	// std::vector <Cylinder> cylinders;
    	cylinders.clear();
-    cylinder_segmentation( cylinders, clusters, T[i], parameters ); 
+    cylinder_segmentation( cylinders, clusters, T[i], parameters );
 
     // Update MAP and save frame
     UpdateMAP_saveFrame( cylinders, frames, landmarks, T[i], parameters );
@@ -113,23 +112,18 @@ int main (int argc, char** argv)
     viewer.removeAllPointClouds();
 
     // Update cloud original - White cloud
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> 
-                                    white_color (cloud_original, 255, 255, 255);                                
-    viewer.addPointCloud <pcl::PointXYZ>(cloud_original, white_color, 
-                                                       "cloud_original");    
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> white_color (cloud_original, 255, 255, 255);
+    viewer.addPointCloud <pcl::PointXYZ>(cloud_original, white_color, "cloud_original");
 
     // Update the cylinders
-    for (std::vector< Cylinder >::iterator 
-                                      it= cylinders.begin();
-                                      it != cylinders.end(); ++it)
+    for (std::vector< Cylinder >::iterator it= cylinders.begin(); it != cylinders.end(); ++it)
     {
-      cloud_cylinder_id= "cloud_cylinder_" + 
-                          patch::to_string( std::distance(cylinders.begin(), it) );
-      pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> 
-                          red_color ( it->cloud, 255, 0, 0);
+      cloud_cylinder_id= "cloud_cylinder_" + patch::to_string( std::distance(cylinders.begin(), it) );
+
+      pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> red_color ( it->cloud, 255, 0, 0);
+
       viewer.addPointCloud<pcl::PointXYZ> (it->cloud, red_color, cloud_cylinder_id);
-      viewer.setPointCloudRenderingProperties 
-                  (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 10, cloud_cylinder_id);
+      viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 10, cloud_cylinder_id);
 
       cout<< "ploting "<< cloud_cylinder_id<< " with #points "<< it->cloud->points.size()<<  endl;
     }
@@ -137,10 +131,12 @@ int main (int argc, char** argv)
     viewer.spinOnce ();
     // ----------------------------  VISUALIZATION  ----------------------------//
 
+    // if the views are ran on a different thread, pausing execution will not affect the ui, for now this should work
+    do {
+      cout << '\n' << "Press a key to continue";
+    } while(cin.get() != '\n');
 
-
-
-    std::cout<< "\n \n \n \n \n \n" << std::endl; 
+    std::cout<< "\n \n \n \n \n \n" << std::endl;
   }
 
 
@@ -154,18 +150,18 @@ int main (int argc, char** argv)
             landmarks[i].pose[0], landmarks[i].pose[1], landmarks[i].rep );
   }
 
-  cout<< "\n \n \n" << std::endl; 
+  cout<< "\n \n \n" << std::endl;
 
   // Show the repeatability per frame
   printf("#frame\t #features\t #detect\t #expect\t rep \n");
   for (int i = 0; i < frames.size(); ++i)
   {
     printf("%i)\t %d\t\t %d\t\t %d\t\t %.2f \n", i, frames[i].numFeatures,
-                            frames[i].numDetected, 
+                            frames[i].numDetected,
                             frames[i].numExpected, frames[i].repRate);
   }
-  
-  cout<< "\n \n \n" << std::endl; 
+
+  cout<< "\n \n \n" << std::endl;
 
   // Show the velo measurements in each frame
   for (int i = 0; i < frames.size(); ++i)
@@ -173,23 +169,23 @@ int main (int argc, char** argv)
     printf("%i) z_velo\t \n", i);
     for (int j = 0; j < frames[i].numFeatures ; j++)
     {
-      printf("%-.2f\t  %-.2f\t %-.2f\t %-.2f\t %-.2f\t \n", 
+      printf("%-.2f\t  %-.2f\t %-.2f\t %-.2f\t %-.2f\t \n",
              frames[i].z_velo[j][0], frames[i].z_velo[j][1], frames[i].z_velo[j][2],
              frames[i].z_velo[j][3], frames[i].z_velo[j][4]);
     }
   }
 
-  cout<< "\n \n \n" << std::endl; 
+  cout<< "\n \n \n" << std::endl;
 
   // Show the nav measurements in each frame
-  cout<< "\n \n \n" << std::endl; 
+  cout<< "\n \n \n" << std::endl;
 
   for (int i = 0; i < frames.size(); ++i)
   {
     printf("%i) z_nav\t \n", i);
     for (int j = 0; j < frames[i].numFeatures ; j++)
     {
-      printf("%-.2f\t  %-.2f\t %-.5f\t %-.5f\t %-.5f\t \n", 
+      printf("%-.2f\t  %-.2f\t %-.5f\t %-.5f\t %-.5f\t \n",
              frames[i].z_nav[j][0], frames[i].z_nav[j][1], frames[i].z_nav[j][2],
              frames[i].z_nav[j][3], frames[i].z_nav[j][4]);
     }
